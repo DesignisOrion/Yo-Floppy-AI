397453e9aebe367858f98b605ef6c54f2da10055
DesignIsOrion <DesignIsOrion@gmail.com>
2021-08-18 14:06:06 -0400

    adding scribe.py for sr

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Yo Floppy!  (AI Assistant)
-GLDNF Voice Assistant Software
+GLDNF Voice Assistant Software that allows automation of weather insight and gaming news using APIs.
 
 
 
--- scribe.py
+++ scribe.py
@@ -1,4 +1,6 @@
 import pyttsx3 as p
+import speech_recognition as sr
+import datetime
 
 engine = p.init()
 
@@ -10,3 +12,15 @@ engine.setProperty('voice', voices[1].id) # using female voice [1]. Male voice [
 print(voices)
 engine.say("I am your YO FLOPPY assistant. How can I help you today!")
 engine.runAndWait() # time waiting for the voice to finish
+
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    r.energy_threshold=10000
+    r.adjust_for_ambient_noise(source,1.2)
+    print("Listening...")
+    text=r.recognize_google(audio)
+    audio = r.listen(source)
+    print(text)
+    
